9af88cfff5a6b709bfe0bc23ca545fa7ef12590e
fletchc <fletcher@colli.nz>
2022-10-19 12:49:55 -0600

    implement basic graph lib

--- graph.c
+++ graph.c
@@ -4,5 +4,72 @@
 
 #include "graph.h"
 
-Matrix *graph_make(uint32_t vertex_count);
-uint32_t graph_set_edge(uint32_t v1, uint32_t v2, uint8_t edge_val);
+typedef edge_t **AdjMatrix;
+
+AdjMatrix adj_matrix_make(uint32_t vertex_count) {
+    AdjMatrix adj_matrix = calloc(vertex_count, sizeof(uint8_t *));
+    if(adj_matrix == NULL) {
+        return NULL;
+    }
+
+    uint32_t max_edges = ((vertex_count - 1) * vertex_count) / 2;
+    uint8_t *adj_matrix_raw = calloc(max_edges, sizeof(uint8_t));
+
+    uint32_t edge_i = 0
+    for(uint32_t i = 0; i < vertex_count; i++) {
+        adj_matrix[i] = adj_matrix_raw + edge_i;
+        edge_i += vertex_count - i - 1;
+    }
+
+    return adj_matrix;
+}
+
+void adj_matrix_free(Graph *graph) {
+    AdjMatrix adj_matrix = graph->adj_matrix;
+    free(*adj_matrix);
+    free(adj_matrix);
+}
+
+Graph *graph_make(uint32_t vertex_count) {
+    Graph *graph = calloc(1, sizeof(Graph));
+    graph->vertex_count = vertex_count;
+    graph->adj_matrix = adj_matrix_make(vertex_count);
+
+    return graph;
+}
+
+void graph_free(Graph *graph) {
+    adj_matrix_free(graph);
+    free(graph);
+}
+
+uint8_t graph_validate_edge(Graph *graph, uint32_t *v1, uint32_t *v2) {
+    if(v1 >= graph->vertex_count || v2 >= graph->vertex_count || v1 == v2) {
+        return -1;
+    }
+    
+    if(v1 > v2) {
+        edge_t tmp = *v1;
+        *v1 = *v2;
+        *v2 = tmp;
+    }
+
+    return 0;
+}
+
+edge_t graph_get_edge(Graph *graph, uint32_t v1, uint32_t v2) {
+    if(graph_validate_edge(graph, &v1, &v2) == -1) {
+        return E_NONE;
+    }
+
+    return graph->adj_matrix[v1][v2];
+}
+
+uint32_t graph_set_edge(Graph *graph, uint32_t v1, uint32_t v2, edge_t edge_val) {
+    if(graph_validate_edge(graph, &v1, &v2) == -1) {
+        return -1;
+    }
+
+    graph->adj_matrix[v1][v2] = edge_val;
+    return 0;
+}
--- graph.h
+++ graph.h
@@ -1,11 +1,13 @@
 #pragma once
 
-typedef uint8_t **AdjMatrix;
+typedef enum edge_vals { E_EXISTS, E_EMPTY, E_NONE } edge_t;
 
 typedef struct {
     uint32_t vertex_count;
     AdjMatrix adj_matrix;
 } Graph;
 
-Matrix *graph_make(uint32_t);
-uint32_t graph_set_edge(uint32_t, uint32_t, uint8_t);
+Graph *graph_make(uint32_t);
+void graph_free(Graph *);
+edge_t graph_get_edge(Graph *, uint32_t, uint32_t);
+uint32_t graph_set_edge(Graph *, uint32_t, uint32_t, uint8_t);
